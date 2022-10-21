55b50d1e8e5f9d1a252bde499a8b9366f3828294
hnam19498 <hoangnam.cntt@outlook.com>
2022-10-21 16:35:05 +0700

    fix

--- python_django/myweb/demoweb/wsgi.py
+++ python_django/myweb/demoweb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for testweb project.
+WSGI config for demoweb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demoweb.settings')
 
 application = get_wsgi_application()
--- python_django/myweb/manage.py
+++ python_django/myweb/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testweb.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demoweb.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
